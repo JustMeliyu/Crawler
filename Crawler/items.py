a16789af35f5f48ac1f441fc5f3be96cac1c5da9
JustMeliyu <642464509@qq.com>
2019-05-29 21:42:35 +0800

    add bookItem&baidu demo

--- Crawler/items.py
+++ Crawler/items.py
@@ -14,3 +14,16 @@ class CrawlerItem(scrapy.Item):
     title = scrapy.Field()
     link = scrapy.Field()
     desc = scrapy.Field()
+
+
+class BookItem(scrapy.Item):
+    # 豆瓣书籍信息
+    book_name = scrapy.Field()
+    auth_name = scrapy.Field()
+    press = scrapy.Field()
+    publication_date = scrapy.Field()
+    price = scrapy.Field()
+    point = scrapy.Field()
+    person_num = scrapy.Field()
+    resume = scrapy.Field()
+    book_detail_url = scrapy.Field()
--- Crawler/spiders/demo.py
+++ Crawler/spiders/demo.py
@@ -7,27 +7,43 @@ __date__ = "19-5-29"
 Describe:
 """
 import scrapy
+from scrapy.spiders.crawl import CrawlSpider, Rule
 from tools.logger import logger
 from Crawler.items import CrawlerItem
+from bs4 import BeautifulSoup
 
 
 class DmozSpider(scrapy.Spider):
     name = "dmoz"
     allowed_domains = ["chinadmoz.org"]
-    start_urls = [
-        "http://www.chinadmoz.org/",
-        "http://www.dmozdir.org/"
-    ]
+    OffsiteMiddleware = True
+    # start_urls = [
+    #     "http://www.chinadmoz.org/",
+    #     "http://www.dmozdir.org/"
+    # ]
+
+    def start_requests(self):
+        yield scrapy.Request('http://www.chinadmoz.org/', self.parse)
+        yield scrapy.Request('http://www.dmozdir.org/', self.parse)
+
+    def logged_in(self, response):
+        # here you would extract links to follow and return Requests for
+        # each of them, with another callback
+        pass
 
     def parse(self, response):
         filename = response.url.split("/")[-2]
+        content = response.body
+        soup = BeautifulSoup(content, "lxml")
+        logger.info("================={0}".format(soup.title.string))
+
+        ci = CrawlerItem()
+        title = response.xpath('/html/head/title/text()').extract()
+        ci['title'] = title
+        logger.info("111111111111111111{0}".format(title))
+
         with open(filename, 'wb') as f:
-            f.write(response.body)
-            ci = CrawlerItem()
-            title = response.xpath('/html/head/title/text()').extract()
-            ci['title'] = title
-            logger.info("111111111111111111{0}".format(title))
-            yield ci
+            f.write(content)
 
 
 if __name__ == '__main__':
